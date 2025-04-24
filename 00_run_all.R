4d4a4d38e98b0e67650f237664ae3fd535c5d603
Cédric Scherer <cedricphilippscherer@gmail.com>
2025-04-24 21:54:25 +0200

    add scripts and project files

---
+++ 00_run_all.R
@@ -0,0 +1,42 @@
+## SCRIPT TO RUN ALL REPORTS
+## WARNING: LINE 39 REMOVES ALL EXISTING HTML REPORTS!
+
+library(quarto)
+library(fs)
+library(stringr)
+library(purrr)
+library(here)
+
+src <- here("R")
+out <- here("docs", "reports")
+
+render_report <- function(input_file, input_dir = src, output_dir = out, ...) {
+  # render the input document to the current working directory
+  quarto::quarto_render(input = paste0(input_dir, "/", input_file), output_format = "html", ...)
+  
+  # name of the rendered output file
+  output_name <- stringr::str_remove(input_file, '.qmd')
+  
+  # move the file to the output path
+  dir_files <- paste0(input_dir, "/", output_name, "_files")
+  dir_cache <- paste0(input_dir, "/", output_name, "_cache")
+  
+  if (file.exists(dir_files)) fs::file_move(dir_files, output_dir)
+  if (file.exists(dir_cache)) fs::file_move(dir_cache, output_dir)
+  fs::file_move(paste0(input_dir, "/", output_name, ".html"), output_dir)
+  
+  msg <- paste0(output_name, ".html moved to ", output_dir)
+  message(msg)
+}
+
+## files to render
+qmds <- list.files(path = src, pattern = ".qmd$")
+
+## render single report
+# render_report(qmds[4])
+
+## render all reports
+## WARNING: THIS PART REMOVES ALL EXISTING HTML REPORTS!
+unlink(out, recursive = TRUE)
+dir.create(out)
+walk(qmds, ~render_report(.x))
